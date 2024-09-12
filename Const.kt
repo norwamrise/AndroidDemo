433f84118bf570770a4fca328cca7cea5e9bce4a
norwamrise <norwamrise@gmail.com>
2024-09-12 09:03:01 +0530

    Create Const.kt

---
+++ Const.kt
@@ -0,0 +1,13 @@
+package com.proeffect.livevoicechanger.utils
+
+object Const {
+    const val NUMBER = "NUMBER"
+    const val AI_AGENT_NAME = "AI_AGENT_NAME"
+    const val USER_PREF = "USER_PREF"
+    const val IS_SUBSCRIPTION = "IS_SUBSCRIPTION"
+    const val FILE_PATH = "FILE_PATH"
+    const val MAIN_FOLDER_NAME = "Direct Voice Changer"
+    const val SUB_FOLDER_NAME = "Changed Voices"
+    const val AD_CONFIG = "AD_CONFIG"
+    const val IS_REVIEW_DONE = "IS_REVIEW_DONE"
+}
